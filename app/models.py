7f84128e12d5faf02f4adefdf805c57fad3ed31e
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-08-08 22:17:09 -0400

    Added in profile page

--- app/models.py
+++ app/models.py
@@ -3,12 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db
 from app import login
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def set_password(self, password):
@@ -17,6 +20,9 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
