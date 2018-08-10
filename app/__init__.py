764cdcd3bb7496fd4bd0f8e161330a1361c0a509
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-08-10 12:19:40 -0400

    Added in reset capability

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -12,6 +13,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
 login.login_view = 'login'
 
 from app import routes, models, errors
